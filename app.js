eb21e52bbea2a335408f56d39539996c2767beea
kuyucucaner <tahacanerkuyucu@gmail.com>
2025-05-26 16:08:59 +0300

    update

--- app.js
+++ app.js
@@ -1,55 +1,64 @@
-var createError = require('http-errors');
-var express = require('express');
-require('express-async-errors'); // Hata izleme kütüphanesi
-var path = require('path');
-var cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-var logger = require('morgan');
-const session = require('express-session');
-const flash = require('express-flash');
-const dotenv = require('dotenv');
+const http = require("http");
+var createError = require("http-errors");
+var express = require("express");
+require("express-async-errors"); // Hata izleme kütüphanesi
+var path = require("path");
+var cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+var logger = require("morgan");
+const session = require("express-session");
+const flash = require("express-flash");
+const dotenv = require("dotenv");
 dotenv.config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(session({ 
-  secret: process.env.SESSION_SECRET_KEY, 
-  resave: true, 
-  saveUninitialized: true,
-  // Diğer seçenekler buraya eklenebilir
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: true,
+    saveUninitialized: true,
+    // Diğer seçenekler buraya eklenebilir
+  })
+);
 
 app.use(flash());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-module.exports = app;
+const port = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
